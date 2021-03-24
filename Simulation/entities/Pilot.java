d40e35f9686fe8eab1745307819cd9238ac13d2b
Diogo <diogo.fernandes77@ua.pt>
2021-03-24 18:41:34 +0000

    Pilot and Plane constructors, Start class accepts configuration settings

--- Simulation/entities/Pilot.java
+++ Simulation/entities/Pilot.java
@@ -4,7 +4,19 @@ package Simulation.entities;
 public class Pilot extends Thread{
 
     
+    public enum State{
+        AT_TRANSFER_GATE,
+        READY_FOR_BOARDING,
+        WAIT_FOR_BOARDING,
+        FLYING_FORWARD,
+        DEBOARDING,
+        FLYING_BACK
+    }
+    private State pilot_state;
 
+    public Pilot(){
+        pilot_state = State.AT_TRANSFER_GATE;
+    }
 
 
 }
--- Simulation/locations/Plane.java
+++ Simulation/locations/Plane.java
@@ -1,5 +1,37 @@
 package Simulation.locations;
 
+import Simulation.Start;
+import Simulation.entities.*;
+import java.util.ArrayList;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 public class Plane{
+    // static variable single_instance of type Singleton
+    private static Plane plane_instance = null;
 
+    private ArrayList<Passenger> plane;
+    private Pilot pilot;
+    private boolean flying;
+    
+    private final Lock lock;
+    //private final Condition arrived;
+   
+    private Plane(){
+        plane = new ArrayList<Passenger>();
+        flying = false;
+        lock = new ReentrantLock();
+        //arrived = lock.newCondition();
+        
+    }
+
+    // static method to create instance of Singleton class
+    public static Plane getInstance()
+    {
+        if (plane_instance == null)
+        plane_instance = new Plane();
+
+        return plane_instance;
+    }
+    
 }
